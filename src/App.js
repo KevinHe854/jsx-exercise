51f3825c5c73c9b9b26a487b60ad258de95a3c92
Tengof <vincent.fong@ucalgary.ca>
2025-03-07 11:42:05 -0700

    Exercise3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import EngineeringTopics from './EngineeringTopics'
 
 function App() {
   return (
     <div className="App">
       <h1>ENSF-381: Full Stack Web Development</h1>
       <p>React Components</p>
+      <EngineeringTopics />
     </div>
+    
   );
 }
+
+
 
 export default App;
