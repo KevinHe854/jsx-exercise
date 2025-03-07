90c7e42c39de8060363053dda4dd57f3552b2f3a
KevinHe854 <kevin.he1@ucalgary.ca>
2025-03-07 11:22:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const currentYear = new Date().getFullYear()
+  var isLoggedIn = true
+
   return (
     <div className="App">
       <h1>ENSF-381: Full Stack Web Development</h1>
       <p>React Components</p>
+      <p>{currentYear}</p>
+      <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
     </div>
   );
 }
